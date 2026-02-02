16a69c4e13736a6a11e03d4cc9e5347d95957ad3
june <mhn06005@gmail.com>
2026-02-02 11:27:05 +0900

    fix: cleanup salute_node after cherry-pick

--- src/cobot2/cobot2/salute_node.py
+++ src/cobot2/cobot2/salute_node.py
@@ -45,10 +45,7 @@ class SaluteRunner(Node):
         # ---- pub/sub ----
         self.sub = self.create_subscription(Bool, self.trigger_topic, self._on_trigger, 10)
         self.pub_done = self.create_publisher(Bool, self.done_topic, 10)
-<<<<<<< HEAD
 
-=======
->>>>>>> f4e0366 (fix: file conflict)
         # ---- state ----
         self._busy = False
         self._lock = threading.Lock()
